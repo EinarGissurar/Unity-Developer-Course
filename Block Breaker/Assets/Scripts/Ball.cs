1154db38406a133d7e906c7084dfb2995e51c838
Einar <einarog05@ru.is>
2018-07-18 13:12:27 +0000

    Moving Block Breaker to repo
    Section 5 from Unity Developer 2D

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball: MonoBehaviour {
+
+    [SerializeField] Paddle paddle;
+    [SerializeField] float vX = 2f; 
+    [SerializeField] float vY = 15f;
+
+    Vector2 paddleToBallV;
+
+    bool start = false;
+
+    // Use this for initialization
+    void Start () {
+        paddleToBallV = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!start)
+        {
+            handleStart();
+        }
+    }
+
+    private void handleStart()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallV;
+        if (Input.GetMouseButtonDown(0))
+        {
+            start = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(vX, vY);
+        }
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenUnitWith = 16f;
+
+    float mousePosX;
+    Vector2 paddlePos;
+
+    // Update is called once per frame
+    void Update () {
+        mousePosX = Mathf.Clamp(Input.mousePosition.x / Screen.width * screenUnitWith, minX, maxX);
+        paddlePos = new Vector2(mousePosX, transform.position.y);
+        transform.position = paddlePos;
+	}
+}
