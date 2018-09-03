57bc0175b5f1355ac0370d8be25e633372d33340
Einar <einarog05@ru.is>
2018-09-03 19:13:08 +0000

    CU3D - Lecture 86
    Complete C# Unity Developer 3D - Progressed to Lecture 86

---
+++ Argon Assault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start() {
+        Invoke("LoadFirstScene", 5f);
+    }
+
+    void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Argon Assault/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    [SerializeField] float xSpeed = 5f;
+    [SerializeField] float ySpeed = 5f;
+    [SerializeField] float xScreenRange = 5f;
+    [SerializeField] float yScreenRange = 4.5f;
+
+    private float horizontalMovement;
+    private float verticalMovement;
+    private float deltaX, deltaY;
+    private float localX, localY;
+
+    // Update is called once per frame
+    void Update () {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        deltaX = horizontalMovement * xSpeed * Time.deltaTime;
+        deltaY = verticalMovement * ySpeed * Time.deltaTime;
+
+        localX = Mathf.Clamp((deltaX + transform.localPosition.x), -xScreenRange, xScreenRange);
+        localY = Mathf.Clamp((deltaY + transform.localPosition.y), -yScreenRange, yScreenRange);
+
+        transform.localPosition = new Vector3(localX, localY, transform.localPosition.z);
+
+        if (Input.GetButtonDown("Fire1")) {
+            Debug.Log("Blem!");
+        }
+	}
+}
